cf8568a83bbe1671095e6409251fd8022de460f3
Matheus Pereira <matttalves@gmail.com>
2022-06-23 00:33:37 -0300

    feat: base setup (nodemon, express, thunder-client)

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (request, response) => {
+    response.send(JSON.stringify({ message: 'Welcome to MYCONTACTS API' }));
+});
+
+app.listen(3000, () => console.log('Server started at http://localhost:3000'));
