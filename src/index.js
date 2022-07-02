4bc0d54e1c0edd582ce77537b87bf21dbf983ee7
Matheus Pereira <matttalves@gmail.com>
2022-07-02 12:55:59 -0300

    feat: eslint, editorconfig

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-    response.send(JSON.stringify({ message: 'Welcome to MYCONTACTS API' }));
+  response.send(JSON.stringify({ message: 'Welcome to MYCONTACTS API' }));
 });
 
 app.listen(3000, () => console.log('Server started at http://localhost:3000'));
