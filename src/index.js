d91183c6ba652a81c0accfcc298aa2573722a7a5
Matheus Alves Pereira <matttalves@gmail.com>
2022-07-19 20:28:40 -0300

    feat: introducing middleware

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const config = require('../package.json');
 const routes = require('./routes');
 
 const app = express();
 
+app.use((request, response, next) => {
+  request.appVersion = config.version;
+  next();
+});
+
 app.get('/', (request, response) => {
-  response.json({ message: 'Welcome to MYCONTACTS API' });
+  response.json({ message: `Welcome to MYCONTACTS API ${request.appVersion}` });
 });
 
 app.use(routes);
