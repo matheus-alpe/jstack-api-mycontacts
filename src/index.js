a66f4c3632f52efb1cd8eb6b74390b5f2ac05e1c
Matheus Alves Pereira <matheus.pereira@dotgroup.com.br>
2022-07-28 21:21:23 -0300

    feat: error handler middleware and async errors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+require('express-async-errors');
+
 const config = require('../package.json');
 const routes = require('./routes');
 
@@ -15,5 +17,11 @@ app.get('/', (request, response) => {
 
 app.use(express.json());
 app.use(routes);
+
+// Error Handler Middleware
+app.use((error, request, response, next) => {
+  console.log(error);
+  response.sendStatus(500);
+});
 
 app.listen(3000, () => console.log('Server started at http://localhost:3000'));
